7c25f20ed834922dc88c3bf00c2fd3d9040f94e3
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-07-30 21:14:30 +0600

    Protected Route

--- src/App.js
+++ src/App.js
@@ -15,8 +15,13 @@ function App() {
       <Routes>
          <Route path="/" element={<Home></Home>}></Route>
          <Route path="/login" element={<Login></Login>}></Route>
-         <Route path='/products' element={<Products></Products>}></Route>
          <Route path="/register" element={<Register></Register>}></Route>
+         <Route path='/products' element={
+          <RequireAuth>
+            <Products></Products>
+          </RequireAuth>
+         }></Route>
+    
          <Route path='/orders' element={
           <RequireAuth>
             <Orders></Orders>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,6 +2,7 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import app from '../../firebase.init';
+import {useLocation, useNavigate} from 'react-router-dom';
 
 
 const auth = getAuth(app);
@@ -9,12 +10,23 @@ const auth = getAuth(app);
 const Login = () => {
     
     const  [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const location = useLocation();
+    const navigate  = useNavigate();
+    const from = location?.state?.from?.pathname || '/';
+
+
+    const handleGoogleSignIN =() => {
+        signInWithGoogle()
+        .then(user =>{
+             navigate(from, {replace:true})
+        })
+    }
 
     return (
         <div>
             <h2>This is Login Page</h2>
             <div style={{margin:'20px'}}>
-                 <button onClick={() => signInWithGoogle()}>Google Sign In</button>
+                 <button onClick={handleGoogleSignIN}>Google Sign In</button>
             </div>
            
             <form >
